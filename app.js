3cbbe913c7f2f753de457efadbd811cd8d0afa51
Beksultan <kenjebekovbeksultan@gmail.com>
2020-06-18 12:38:24 +0600

    app.js, products.js, orders,js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Import Routes
+const productRoute = require('./routes/products');
+//const usersRoute = require('./routes/users');
+const orderRoute = require('./routes/orders');
 
-var app = express();
+// Use Routes
+//app.use('/api/users', usersRoute);
+app.use('/api/products', productRoute);
+//app.use('/api/auth', authRoute);
+app.use('/api/orders', orderRoute);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+  allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}))
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +30,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
