74e433cfe54c9c590f2d78646381cf5a198f4f72
Beksultan <kenjebekovbeksultan@gmail.com>
2020-06-21 22:09:04 +0600

    Order and Cart Service - Part 1

--- routes/products.js
+++ routes/products.js
@@ -59,6 +59,7 @@ router.get('/:prodId',(req, res) => {
       .withFields(['c.title as category',
         'p.title as name',
         'p.price',
+        'description',
         'p.quantity',
         'p.image',
         'p.images',
